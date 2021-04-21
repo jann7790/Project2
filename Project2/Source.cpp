4cb9cfcbb62fc7295d55cc7c30d725d543f11e2e
jann7790 <jann7790@gmail.com>
2021-04-22 00:34:59 +0800

    add update

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -3,5 +3,6 @@
 
 int main()
 {
-	;
+	bignum num;
+	num.input();
 }
--- Project2/bignum.cpp
+++ Project2/bignum.cpp
@@ -1,14 +1,30 @@
 #include "bignum.h"
-void bignum::input(string& input)
+void bignum::input()
 {
-    numbers = input;
+    cin >> numbers;
 }
 bignum& bignum::operator+(bignum& rhs)
 {
     if (numbers.length() > rhs.numbers.length())
     {
-        numbers.insert(0, '0', numbers.length() - rhs.numbers.length());
+        numbers.insert(0, numbers.length() - rhs.numbers.length(), '0');
     }
+    else
+    {
+        rhs.numbers.insert(0, rhs.numbers.length() - numbers.length(), '0');
+    }
+    string result = "";
+    int temp;
+    int carry = 0;
+    for (size_t i = 0; i < numbers.length(); i++)
+    {
+        temp = (rhs.numbers[numbers.length() - i - 1] + numbers[numbers.length() - i - 1]) - '0' - '0' + carry;
+        carry = temp / 10;
+        temp %= 10;
+        result.insert(0, 1, temp + '0');
+    }
+    if (carry)
+        result.insert(0, 1, carry + '0');
     return *this;
 }
 
